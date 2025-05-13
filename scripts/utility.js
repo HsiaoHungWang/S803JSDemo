11fd3bac1a41dbe843eb3ad0d5ec8fe75d868d58
HsioaHungWang <wang@ispan.com.tw>
2025-05-13 15:56:27 +0800

    變數有效範圍

--- scripts/utility.js
+++ scripts/utility.js
@@ -2,6 +2,7 @@ const celsius_to_fahrenheit = celsius => celsius * (9 / 5) + 32;
 
 const fahrenheit_to_celsius = fahrenheit => (fahrenheit - 32) * 5 / 9;
 
+//純函式
 const tempatureConvert = (degree, type) => {
     let degreeConvert;
     if (type === 'C') {
