2229a886f841518087302ae66acb6dd2fd81cc13
HsioaHungWang <wang@ispan.com.tw>
2025-05-14 14:07:46 +0800

    RegExp 練習

--- scripts/validate.js
+++ scripts/validate.js
@@ -28,5 +28,52 @@ const checkMail = (email) => {
     return isValid;
 }
 
+const checkMailByRegExp = email => {
+    let isValid = true;
+    const re = /^.+@.+\..{2,3}$/;
+    if (!re.test(email)) {
+        isValid = false;
+    }
+    return isValid;
 
-export { checkEmpty, checkMail };
+}
+
+
+//身分證的格式檢查a 1 23456789
+const chkPersonId = id => {
+    let isValid = true;
+    const re = /^[a-z]{1}[1-2]{1}\d{8}$/;
+    if (!re.test(id)) {
+        isValid = false;
+    }
+    return isValid;
+}
+
+
+//手機號碼格式檢查 0911-222-333
+const checkCellPhone = cell => {
+    let isValid = true;
+    const re = /^09\d{2}-\d{3}-\d{3}$/;
+    if (!re.test(cell)) {
+        isValid = false;
+    }
+    return isValid;
+}
+
+// 至少 8 個字元
+// 至少有以下四個內容的其中三個
+// 小寫字母
+// 大寫字母
+// 數字
+// 特殊符號
+const checkPassword = pwd => {
+    let isValid = true;
+    const re = /^((?=.{8,}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*|(?=.{8,}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!\u0022#$%&'()*+,./:;<=>?@[\]\^_`{|}~-]).*)/;
+    if (!re.test(pwd)) {
+        isValid = false;
+    }
+    return isValid
+
+}
+
+export { checkEmpty, checkMail, chkPersonId, checkCellPhone, checkMailByRegExp, checkPassword };
